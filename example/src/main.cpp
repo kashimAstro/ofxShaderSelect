594f75f386127a28e66df2e79ce8987964112c55
kashim <kashim@kashim-laptop.x>
2015-11-22 13:33:41 +0100

    update add new shader demostration tessellation GLSL 440

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -1,14 +1,43 @@
 #include "ofMain.h"
 #include "ofxShaderSelect.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
 	public:
 
 	ofxShaderSelect shader;
+    ofShader pass;
+    ofEasyCam cam;
+    ofIcoSpherePrimitive ico;
+    ofSpherePrimitive sphere;
+    ofBoxPrimitive box;
+
+    ofVbo vbo;
+    ofVec3f normals;
+    ofxPanel gui;
+    ofParameter<int> levelInner;
+    ofParameter<int> levelOuter;
+    ofParameter<ofVec3f> lightpos;
+    ofParameter<ofVec3f> diffMat;
+    ofParameter<ofVec3f> ambMat;
 
 	void setup(){
 		vector<string> shaders = shader.load("shader.glsl");
-		shader.active(shaders);                
+        pass = shader.active(shaders,440);
+
+        gui.setup();
+        gui.add(levelInner.set("level inner", 1, 1, 8));
+        gui.add(levelOuter.set("level outer", 1, 1, 8));
+        gui.add(lightpos.set("light pos",      ofVec3f(0.),                 ofVec3f(0.), ofVec3f(50.)));
+        gui.add(diffMat.set("diffuse material",ofVec3f(0.04f, 0.04f, 0.04f),ofVec3f(0.), ofVec3f(1.)));
+        gui.add(ambMat.set("ambient material", ofVec3f(0, 0.75, 0.75),      ofVec3f(0.), ofVec3f(1.)));
+
+        cam.setNearClip(.1);
+        cam.setFarClip(100000);
+
+        ico.set(100,1);
+        ofMesh m = ico.getMesh();
+        vbo.setMesh(m,GL_DYNAMIC_DRAW);
 	}
 
 	void update(){
@@ -16,11 +45,38 @@ class ofApp : public ofBaseApp{
 	}
 
 	void draw(){
+        ofBackground(0,0,95);
+        cam.begin();
+        ofEnableDepthTest();
+        pass.begin();
 
+        pass.setUniform3f("LightPosition",lightpos->x,lightpos->y,lightpos->z);
+        pass.setUniform3f("DiffuseMaterial",diffMat->x,diffMat->y,diffMat->z);
+        pass.setUniform3f("AmbientMaterial",ambMat->x,ambMat->y,ambMat->z);
+        pass.setUniformMatrix4f("Modelview",cam.getModelViewMatrix());
+        pass.setUniformMatrix3f("NormalMatrix",ofMatrix3x3(0.1,0.5,1.0,0.1,0.,0.,0.,0.,0.));
+        pass.setUniform1f("TessLevelInner",levelInner);
+        pass.setUniform1f("TessLevelOuter",levelOuter);
+        pass.setUniformMatrix4f("Projection",cam.getProjectionMatrix());
+
+            vbo.drawElements(GL_PATCHES, vbo.getNumVertices()*vbo.getNumVertices());
+
+        pass.end();
+        ofDisableDepthTest();
+        cam.end();
+
+        gui.draw();
 	}
+
+    void keyPressed(int key){
+        if(key == ' '){
+            pass.printActiveAttributes();
+            pass.printActiveUniforms();
+        }
+    }
 };
 
 int main( ){
-	ofSetupOpenGL(1024,768, OF_WINDOW);
-	ofRunApp( new ofApp());
+    ofSetupOpenGL(1280,700,OF_WINDOW);
+    ofRunApp( new ofApp());
 }
--- src/ofxShaderSelect.h
+++ src/ofxShaderSelect.h
@@ -16,18 +16,17 @@ class ofxShaderSelect{
 
 	vector<string> load(string path){
 		bufferShader.clear();
-		file.open(path, ofFile::ReadWrite, false);
-                ofBuffer buff = file.readToBuffer();
-                vector<string> source = ofSplitString(buff.getText(),"--");
-
-                for(int i = 0; i < source.size(); i++) {
+        file.open(path, ofFile::ReadWrite, false);
+        ofBuffer buff = file.readToBuffer();
+        vector<string> source = ofSplitString(buff.getText(),"--");
+        for(int i = 0; i < source.size(); i++) {
 			if(source[i] != "") {
-				vector<string> split = ofSplitString(source[i],"\n");
+                vector<string> split = ofSplitString(source[i],"\n");
 				for(int j = 0; j < split.size(); j++) {
 					if( i == counter ) {
-						buffer+=split[j];
+                        buffer+=split[j]+"\n";
 					}
-                        	}
+                }
 			}
 			ofStringReplace(buffer, VERTEX,     "");
 			ofStringReplace(buffer, GEOMETRY,   "");
@@ -37,20 +36,24 @@ class ofxShaderSelect{
 			bufferShader.push_back(buffer);
 			counter++;
 			buffer="";
-                }
+        }
 		return bufferShader;
 	}
 
-	void active(vector<string> source, GLenum input=GL_POINTS, GLenum output=GL_TRIANGLE_STRIP, int outputCount=60) {
-	       	shader.setGeometryInputType(input);
-		shader.setGeometryOutputType(output);
-		shader.setGeometryOutputCount(outputCount);
-		for(int i = 0; i < source.size(); i++){
-			ofLog()<<source[i];
-		}
-        //	shader.setupShaderFromSource(GL_VERTEX_SHADER,       source[0]);
-	//      shader.setupShaderFromSource(GL_FRAGMENT_SHADER,     source[1]);
-	//      shader.setupShaderFromSource(GL_GEOMETRY_SHADER_EXT, source[2]);
-        //	shader.linkProgram();
+    ofShader active(vector<string> source, int version, GLenum input=GL_POINTS, GLenum output=GL_TRIANGLE_STRIP, int outputCount=60) {
+        if(source.size()>=4) {
+            shader.setGeometryInputType(input);
+            shader.setGeometryOutputType(output);
+            shader.setGeometryOutputCount(outputCount);
+            ofLog()<<"geometry headers!";
         }
+        ofLog()<<source.size();
+        if(source.size()>=2) { shader.setupShaderFromSource(GL_VERTEX_SHADER,          "\n#version "+ofToString(version)+"\n"+source[1]); ofLog()<<"vertex shader!"; }
+        if(source.size()>=3) { shader.setupShaderFromSource(GL_FRAGMENT_SHADER,        "\n#version "+ofToString(version)+"\n"+source[2]); ofLog()<<"fragment shader!"; }
+        if(source.size()>=4) { shader.setupShaderFromSource(GL_GEOMETRY_SHADER_EXT,    "\n#version "+ofToString(version)+"\n"+source[3]); ofLog()<<"geometry shader!"; }
+        if(source.size()>=5) { shader.setupShaderFromSource(GL_TESS_CONTROL_SHADER,    "\n#version "+ofToString(version)+"\n"+source[4]); ofLog()<<"tess control shader!"; }
+        if(source.size()>=6) { shader.setupShaderFromSource(GL_TESS_EVALUATION_SHADER, "\n#version "+ofToString(version)+"\n"+source[5]); ofLog()<<"tess evaluation shader!"; }
+        if(source.size()>=2) { shader.linkProgram(); ofLog()<<"link program start!"; }
+        return shader;
+    }
 };
